057bcdd5458fc74a758b5a3806342ddadf74deaf
Nicholas Chew <chewshenmin@gmail.com>
2024-07-26 08:04:16 +0800

    Synchronous communication using Spring Cloud OpenFeign

--- order-service/src/main/java/com/niche/microservices/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/niche/microservices/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.niche.microservices.orderservice.service;
 
+import com.niche.microservices.orderservice.client.InventoryClient;
 import com.niche.microservices.orderservice.dto.OrderRequest;
 import com.niche.microservices.orderservice.model.Order;
 import com.niche.microservices.orderservice.repository.OrderRepository;
@@ -14,15 +15,25 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
 
-    public void placeOrder(OrderRequest orderRequest) {
-        // map OrderRequest to order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
+    private final InventoryClient inventoryClient;
 
-        //save order to OrderRepo
-        orderRepository.save(order);
+    public void placeOrder(OrderRequest orderRequest) {
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if (isProductInStock) {
+            // map OrderRequest to order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            //save order to OrderRepo
+            orderRepository.save(order);
+
+        } else {
+            throw new RuntimeException("Product with SkuCode " + orderRequest.skuCode() + " is not in stock");
+
+        }
+
     }
 }
